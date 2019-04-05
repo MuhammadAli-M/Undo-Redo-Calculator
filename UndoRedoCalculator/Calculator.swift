743ad6ee383daaf9522bb86ca3939519201b0e24
Muhammad Adam <muhammadalyadam@gmail.com>
2019-04-06 00:36:57 +0200

    add stack data structure

--- UndoRedoCalculator/Calculator.swift
+++ UndoRedoCalculator/Calculator.swift
@@ -7,3 +7,38 @@
 //
 
 import Foundation
+
+
+
+// MARK: Stack
+
+struct Stack<T> {
+    private var list:[T] = []
+    
+    mutating func push(element: T){
+        list.append(element)
+    }
+    
+    mutating func pop() -> T?{
+        if list.isEmpty {
+            return nil
+        }else{
+            return list.removeLast()
+        }
+    }
+    
+    mutating func peek() -> T?{
+        return list.last
+    }
+    
+    mutating func clear(){
+        return list.removeAll()
+    }
+    var isEmpty: Bool {
+        return list.isEmpty
+    }
+    
+    var count: Int {
+        return list.count
+    }
+}
