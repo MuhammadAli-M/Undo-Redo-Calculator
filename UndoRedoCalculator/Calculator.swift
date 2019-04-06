614df2d4ff5aa05f8e53d428a242998009b7f92d
Muhammad Adam <muhammadalyadam@gmail.com>
2019-04-06 02:34:57 +0200

    add calculator's methods do/undo/redo ; edit Stack struct

--- UndoRedoCalculator/Calculator.swift
+++ UndoRedoCalculator/Calculator.swift
@@ -8,6 +8,44 @@
 
 import Foundation
 
+class Calculator {
+    
+    var undoStack = Stack<Operation>()
+    var redoStack = Stack<Operation>()
+    var undoCount = 0
+    var result = 0
+    
+    
+    func doOperation( operation: inout Operation) {
+        operation.execute()
+        undoStack.push(element: operation)
+        result = operation.result
+        redoStack.clear()
+    }
+    
+    func undo(){
+        undoCount += 1
+        if var op = undoStack[undoStack.count - undoCount] {
+            undoStack.push(element: op.reverse())
+            undoCount += 1
+            redoStack.push(element: op)
+            op.first = result
+            op.unExecute()
+            result = op.result
+        }
+    }
+    
+    func redo(){
+        let op = redoStack.pop()
+        undoCount = 0   // To start undo from the top of undoStack
+        if var op = op {
+            undoStack.push(element: op)
+            op.first = result
+            op.execute()
+            result = op.result
+        }
+    }
+}
 
 
 // MARK: Stack
@@ -40,5 +78,13 @@ struct Stack<T> {
     
     var count: Int {
         return list.count
+    }
+    
+    subscript(_ index:Int) -> T?{
+        if (index >= 0 && index < list.count){
+            return list[index]
+        }else{
+            return nil
+        }
     }
 }
