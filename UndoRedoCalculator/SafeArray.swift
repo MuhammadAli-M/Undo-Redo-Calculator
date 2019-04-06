5156d933f033ef870261a14eb0a48ae8cc0299c1
Muhammad Adam <muhammadalyadam@gmail.com>
2019-04-06 19:38:13 +0200

    add SafeArray methods and properties

--- UndoRedoCalculator/SafeArray.swift
+++ UndoRedoCalculator/SafeArray.swift
@@ -7,3 +7,146 @@
 //
 
 import Foundation
+
+class SafeArray<Element>{
+    fileprivate let queue = DispatchQueue(label: "MA.UndoRedoCalculator.SafeArray", attributes: .concurrent)
+    fileprivate var array:[Element]
+    
+    /// The first element of the collection.
+    var first: Element? {
+        var result: Element?
+        queue.sync { result = self.array.first }
+        return result
+    }
+    
+    /// The last element of the collection.
+    var last: Element? {
+        var result: Element?
+        queue.sync { result = self.array.last }
+        return result
+    }
+    
+    /// The number of elements in the array.
+    var count: Int {
+        var result = 0
+        queue.sync { result = self.array.count }
+        return result
+    }
+    
+    /// A Boolean value indicating whether the collection is empty.
+    var isEmpty: Bool {
+        var result = false
+        queue.sync { result = self.array.isEmpty }
+        return result
+    }
+    
+    
+    init() {
+        array = [Element]()
+    }
+    
+    
+    /// Calls the given closure on each element in the sequence in the same order as a for-in loop.
+    ///
+    /// - Parameter body: A closure that takes an element of the sequence as a parameter.
+    func forEach(_ body: (Element) -> Void) {
+        queue.sync { self.array.forEach(body) }
+    }
+    
+    /// Returns a Boolean value indicating whether the sequence contains an element that satisfies the given predicate.
+    ///
+    /// - Parameter predicate: A closure that takes an element of the sequence as its argument and returns a Boolean value that indicates whether the passed element represents a match.
+    /// - Returns: true if the sequence contains an element that satisfies predicate; otherwise, false.
+    func contains(where predicate: (Element) -> Bool) -> Bool {
+        var result = false
+        queue.sync { result = self.array.contains(where: predicate) }
+        return result
+    }
+    
+    
+    /// Adds a new element at the end of the array.
+    ///
+    /// - Parameter element: The element to append to the array.
+    func append( _ element: Element) {
+        queue.async(flags: .barrier) {
+            self.array.append(element)
+        }
+    }
+    
+    
+    /// Inserts a new element at the specified position.
+    ///
+    /// - Parameters:
+    ///   - element: The new element to insert into the array.
+    ///   - index: The position at which to insert the new element.
+    func insert( _ element: Element, at index: Int) {
+        queue.async(flags: .barrier) {
+            self.array.insert(element, at: index)
+        }
+    }
+    
+    /// Removes and returns the element at the specified position.
+    ///
+    /// - Parameters:
+    ///   - index: The position of the element to remove.
+    ///   - completion: The handler with the removed element.
+    func remove(at index: Int, completion: ((Element) -> Void)? = nil) {
+        queue.async(flags: .barrier) {
+            let element = self.array.remove(at: index)
+            
+            DispatchQueue.main.async {
+                completion?(element)
+            }
+        }
+    }
+    
+    /// Removes last element and return it.
+    ///
+    /// - Returns: optional of last element.
+    func removeLast()->Element? {
+        var result:Element?
+        
+        queue.sync {[weak self] in
+            result = self?.array.last
+        }
+        queue.async(flags: .barrier) {[weak self] in
+            self?.array.removeLast()
+        }
+        return result
+    }
+    
+    /// Removes all elements from the array.
+    ///
+    /// - Parameter completion: The handler with the removed elements.
+    func removeAll(keepingCapacity keepCapacity: Bool = false){
+        queue.async(flags: .barrier) { [weak self] in
+            self?.array.removeAll()
+        }
+    }
+
+    
+    
+    /// Accesses the element at the specified position if it exists.
+    ///
+    /// - Parameter index: The position of the element to access.
+    /// - Returns: optional element if it exists.
+    subscript(index: Int) -> Element? {
+        get {
+            var result: Element?
+            if 0..<array.count ~= index{ //validate the index
+                queue.sync {[weak self] in
+                    result = self?.array[index]
+                }
+            }
+            return result
+        }
+        
+        set {
+            guard let newValue = newValue else { return }
+            queue.async(flags: .barrier) {
+                self.array[index] = newValue
+            }
+        }
+    }
+
+}
