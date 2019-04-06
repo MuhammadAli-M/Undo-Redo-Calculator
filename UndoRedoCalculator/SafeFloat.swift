798f9641a88fcfb645ac4ac49bbfb79756198185
Muhammad Adam <muhammadalyadam@gmail.com>
2019-04-06 21:51:23 +0200

    add SafeFloat methods to make it thread safe

--- UndoRedoCalculator/SafeFloat.swift
+++ UndoRedoCalculator/SafeFloat.swift
@@ -7,3 +7,38 @@
 //
 
 import Foundation
+
+protocol SafeFloatDelegate: class {
+    func safeFloatValueDidSet()
+}
+
+class SafeFloat {
+    private let queue = DispatchQueue(label: "MA.UndoRedoCalculator.SafeFloat", attributes: .concurrent)
+    private var value:Float
+    weak var delegate: SafeFloatDelegate?
+    
+    func set(_ value:Float){
+        queue.async(flags: .barrier) {
+            self.value = value
+        }
+        delegate?.safeFloatValueDidSet()
+    }
+    
+    func get() -> Float{
+        var result = Float()
+        queue.sync{
+            result = value
+        }
+        return result
+    }
+    
+    init() {
+        value = Float()
+    }
+    
+    convenience init(_ value:Float) {
+        self.init()
+        set(value)
+    }
+
+}
