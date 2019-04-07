3b73d1240d9c52db178d17779bf51f92dd599816
Muhammad Adam <muhammadalyadam@gmail.com>
2019-04-07 09:37:53 +0200

    add test case to test complex successive undo and redo operations

--- CalculatorTestCase.swift
+++ CalculatorTestCase.swift
@@ -8,8 +8,9 @@
 
 import XCTest
 
+
 class CalculatorTestCase: XCTestCase {
-    
+    var calculator = Calculator()
     override func setUp() {
         super.setUp()
         // Put setup code here. This method is called before the invocation of each test method in the class.
@@ -20,9 +21,47 @@ class CalculatorTestCase: XCTestCase {
         super.tearDown()
     }
     
-    func testExample() {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    func testComplexUndoAndRedo() {
+        var result = SafeFloat(0)
+        var op1 = Operation(first: result.get(), second: 3, mathOp: .add)
+        calculator.doOperation(operation: &op1)
+        result.set(calculator.result)
+        
+        var op2 = Operation(first: result.get() , second: 2, mathOp: .add)
+        calculator.doOperation(operation: &op2)
+        result.set(calculator.result)
+        
+        var op3 = Operation(first: result.get() , second: 5, mathOp: .mul)
+        calculator.doOperation(operation: &op3)
+        result.set(calculator.result)
+        
+        calculator.undo()
+        result.set(calculator.result)
+        
+        calculator.undo()
+        result.set(calculator.result)
+        
+        calculator.redo()
+        result.set(calculator.result)
+        
+        var op4 = Operation(first: result.get() , second: 3, mathOp: .add)
+        calculator.doOperation(operation: &op4)
+        result.set(calculator.result)
+        
+        calculator.undo()
+        result.set(calculator.result)
+        
+        calculator.undo()
+        result.set(calculator.result)
+        
+        calculator.undo()
+        result.set(calculator.result)
+        
+        calculator.undo()
+        result.set(calculator.result)
+        
+        XCTAssertEqual(result.get(), 25)
+        
     }
     
     func testPerformanceExample() {
